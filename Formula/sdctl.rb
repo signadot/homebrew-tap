63c68f41d8fa69d7e6e525e0c452861979d03b2d
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-29 22:47:58 -0700

    Brew formula update for signadot version v0.5.4-snapshot

--- Formula/sdctl.rb
+++ Formula/sdctl.rb
@@ -10,11 +10,11 @@ class Sdctl < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/signadot/signadot/releases/download/v0.5.4-snapshot/signadot_0.5.4-snapshot_darwin_amd64.tar.gz"
-    sha256 "b4e8cc3406b65364f280b9d55c611dba2dc13d935f30a4fa7aa7dc6a8ef2a14d"
+    sha256 "c0a0f4cac6e241363ac5d7acca35a97a0fd0c465fccead60df96beeb532d057b"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/signadot/signadot/releases/download/v0.5.4-snapshot/signadot_0.5.4-snapshot_linux_amd64.tar.gz"
-    sha256 "ec6ea5ff070599e7dbdeb0b478dc9f61fe2613f8961c78a44f102528e683ca68"
+    sha256 "2895fa3ab4155d25af30e01c4b92b87961b0a93e12576d7a10b3e030b90f39d5"
   end
 
   depends_on "sshuttle"
