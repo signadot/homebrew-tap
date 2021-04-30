4dcb5b0cb9cd6d8f8a3cfb6645b451a6c91079f8
Anirudh Ramanathan <anirudh@foxish.me>
2021-04-29 22:50:57 -0700

    Update sdctl.rb

--- Formula/sdctl.rb
+++ Formula/sdctl.rb
@@ -9,11 +9,11 @@ class Sdctl < Formula
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/signadot/signadot/releases/download/v0.5.4-snapshot/signadot_0.5.4-snapshot_darwin_amd64.tar.gz"
+    url "https://sdctl.s3-us-west-2.amazonaws.com/pre-release/v0.5.4-snapshot/signadot_0.5.4-snapshot_darwin_amd64.tar.gz"
     sha256 "c0a0f4cac6e241363ac5d7acca35a97a0fd0c465fccead60df96beeb532d057b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/signadot/signadot/releases/download/v0.5.4-snapshot/signadot_0.5.4-snapshot_linux_amd64.tar.gz"
+    url "https://sdctl.s3-us-west-2.amazonaws.com/pre-release/v0.5.4-snapshot/signadot_0.5.4-snapshot_linux_amd64.tar.gz"
     sha256 "2895fa3ab4155d25af30e01c4b92b87961b0a93e12576d7a10b3e030b90f39d5"
   end
 
